44fdefa461bd2dcfa78d6ff65bdeb43dff83e49f
Nai <yer2@illinois.edu>
2025-02-26 13:52:58 -0600

    add lab 2

---
+++ workspace/me446_lab_starter/matlab/simulink5ms_plotAndGains_sldrt_win64/rtmodel.h
@@ -0,0 +1,25 @@
+/*
+ *  rtmodel.h:
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "simulink5ms_plotAndGains".
+ *
+ * Model version              : 11.0
+ * Simulink Coder version : 9.9 (R2023a) 19-Nov-2022
+ * C source code generated on : Wed Feb 26 13:40:56 2025
+ *
+ * Target selection: sldrt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Linux 64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rtmodel_h_
+#define RTW_HEADER_rtmodel_h_
+#include "simulink5ms_plotAndGains.h"
+#define GRTINTERFACE                   1
+#endif                                 /* RTW_HEADER_rtmodel_h_ */
